a2010494680507d8ace27df7d6036b4c698c6d7f
XintongCao <100534320+XintongCao@users.noreply.github.com>
2022-03-19 20:35:26 +0000

    Update userClass.py

--- ATM project/Packages/userClass.py
+++ ATM project/Packages/userClass.py
@@ -1,6 +1,6 @@
 class User():
     def __init__(self,name,userid,phone_num,card):
-        self.name=name #用户名
-        self.userid=userid #身份证号
-        self.phone_num=phone_num #手机号
-        self.card=card #银行卡==>对象，cardClass
+        self.name=name #User name
+        self.userid=userid #User ID number
+        self.phone_num=phone_num #phone number
+        self.card=card #Card==>Object，cardClass
