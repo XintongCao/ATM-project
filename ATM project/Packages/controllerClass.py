d378f8a704ce2f50d9dc612a70b9af845e8f4337
XintongCao <100534320+XintongCao@users.noreply.github.com>
2022-03-19 20:34:21 +0000

    Update controllerClass.py

--- ATM project/Packages/controllerClass.py
+++ ATM project/Packages/controllerClass.py
@@ -1,20 +1,20 @@
 import random, pickle,os
 from Packages import cardClass as card
 from Packages import userClass as user
-#操作控制类
+#Operation Control Class
 class Controller():
-    #数据存储格式
-    userid_cardobj_dict=dict() #{身份证ID：银行卡对象}
-    cardid_userobj_dict=dict() #{银行卡ID：用户对象}
-    #数据存储的url
+    #Data storage format
+    userid_cardobj_dict=dict() #{User ID：Card object}
+    cardid_userobj_dict=dict() #{Card number：User object}
+    #url for data storage
     user_file_url='./Databases/user.txt'
     card_file_url='./Databases/card.txt'
-    #1.注册功能
+    #1.Registration function
     def registration(self):
-        #获取用户输入的【用户名】/【身份证号】/【手机号】/【密码】
+        #Obtain【user name】/【user ID】/【phone number】/【PIN】input by the user
         name=self.__getusername()
         userid=self.__getuserid()
-        #检测当前身份证号 是否已经存在
+        #Check if the current ID number already exists
         if userid in self.userid_cardobj_dict:
             print(f'''
 *******************************************************
@@ -28,15 +28,15 @@ class Controller():
         pwd=self.__getpwd()
         print('Name:',name,' User_id:',userid,' Phone number:',phone_num,' Password:',pwd)
 
-        #创建一个银行卡号
+        #Create a bank card number
         cardid=random.randint(10000,99999)
         cardobj=card.Card(cardid,pwd,balance=0,islock=False)
-        #创建用户对象，和银行卡进行绑定
+        #Create a user object and bind it to the bank card
         userobj=user.User(name,userid,phone_num,cardobj)
-        #创建需要保存用户信息 数据格式{身份证号：cardobj},{cardid:userobj}
+        #Create the user information that needs to be saved. Data format is {user ID:cardobj},{cardid:userobj}
         self.userid_cardobj_dict[userid]=cardobj
         self.cardid_userobj_dict[cardid]=userobj
-        #完成创建
+        #Complete the creation
         print(f'''
 ***************************************************
 *                                                 *
@@ -45,28 +45,28 @@ class Controller():
 *                                                 *
 ***************************************************
 ''')
-    #获取所有的用户信息
+    #Get all user information
     def __init__(self):
-        #加载所有数据信息
+        #Load all data information
         self.__loaddata()
 
     def __loaddata(self):
-        #检验文件是否存在
+        #Checking the existence of documents
         if os.path.exists(self.card_file_url):
-            #读取数据
+            #Read information
             with open(self.card_file_url,'rb') as fp:
                 self.userid_cardobj_dict=pickle.load(fp)
                 #print(self.userid_cardobj_dict)
         if os.path.exists(self.user_file_url):
-            #读取数据
+            #Read data
             with open(self.user_file_url,'rb') as fp:
                 self.cardid_userobj_dict=pickle.load(fp)
 
-    #2.查询功能
+    #2.Enquiry function
     def enquiry(self):
-        #获取用户数据的卡号
+        #Get the card number of the user
         cardid=int(input('Please enter your card number: '))
-        #验证卡号是否存在
+        #Check the existence of the card number
         if cardid not in self.cardid_userobj_dict:
             print('''
 ********************************************
@@ -76,11 +76,11 @@ class Controller():
 ********************************************
 ''')
             return
-        #获取卡对象
+        #Get Card Object
         cardobj=self.cardid_userobj_dict[cardid].card
-        #如果存在，则输入密码
+        #If present, enter the password
         if self.__checkpwd(cardobj):
-            #验证是否锁卡
+            #Verify that the card is locked
             if cardobj.islock:
                 print('''
 **********************************************************
@@ -91,7 +91,7 @@ class Controller():
 ''')
                 return
             else:
-                #通过卡号获取当前卡的余额信息
+                #Get the current card balance information by card number
                 print(f'''
 *******************************************************
 *                                                     *
@@ -100,11 +100,11 @@ class Controller():
 *******************************************************
 ''')
 
-    #3.存款功能
+    #3.Deposit function
     def deposit(self):
-        #获取用户数据的卡号
+        #Get the user's card number
         cardid=int(input('Please enter your card number: '))
-        #验证卡号是否存在
+        #Verify the existence of the card number
         if cardid not in self.cardid_userobj_dict:
             print('''
 ********************************************
@@ -114,11 +114,11 @@ class Controller():
 ********************************************
 ''')
             return
-        #获取卡对象
+        #Get Card Object
         cardobj=self.cardid_userobj_dict[cardid].card
-        #如果存在，则验证密码
+        #If present, verify password
         if self.__checkpwd(cardobj):
-            #验证是否锁卡
+            #Verify that the card is locked
             if cardobj.islock:
                 print('''
 **********************************************************
@@ -129,9 +129,9 @@ class Controller():
 ''')
                 return
             else:
-                #获取存款金额
+                #Get deposit amount
                 deposit=int(input('Please enter your deposit amount: '))
-                #存款金额不能小于0
+                #The deposit amount cannot be less than 0
                 if deposit<=0:
                     print('''
 *********************************************************
@@ -151,11 +151,11 @@ class Controller():
 ********************************************
 ''')
 
-    #4.取款功能
+    #4.Withdrawal function
     def withdrawal(self):
-        #获取用户卡号
+        #Obtain user card number
         cardid=int(input('Please enter your card number: '))
-        #验证输入的卡号是否存在
+        #Verify that the card number entered exists
         if cardid not in self.cardid_userobj_dict:
             print('''
 ********************************************
@@ -165,11 +165,11 @@ class Controller():
 ********************************************
 ''')
             return
-        #获取卡对象
+        #Get Card Object
         cardobj=self.cardid_userobj_dict[cardid].card
-        #如果卡存在，则验证密码
+        #Verify password if card exists
         if self.__checkpwd(cardobj):
-            #验证是否锁卡
+            #Verify that the card is locked
             if cardobj.islock:
                 print('''
 **********************************************************
@@ -180,9 +180,9 @@ class Controller():
 ''')
                 return
             else:
-                #获取取款金额
+                #Get the withdrawal amount
                 withdrawal=int(input('Please enter your withdrawal amount: '))
-                #取款金额不能大于卡上现有余额
+                #The withdrawal amount cannot be greater than the existing balance on the card
                 if withdrawal>cardobj.balance:
                     print('''
 ****************************
@@ -202,11 +202,11 @@ class Controller():
 ***********************************************
 ''')
 
-    #5.转账功能
+    #5.Transfer function
     def transfer(self):
-        #获取用户转出的卡号
+        #Get the card number of the user
         cardid=int(input('Please enter your card number: '))
-        #验证输入的转出卡号是否存在
+        #Verify that the card number entered for the transfer exists
         if cardid not in self.cardid_userobj_dict:
             print('''
 ********************************************
@@ -216,14 +216,14 @@ class Controller():
 ********************************************
 ''')
             return
-        #获取收款卡号和收款方姓名
+        #Get the card number and name of the recipient
         cardid_recipient=int(input('Please enter the card number of the recipient: '))
         name_recipient=input('Please enter the name of the recipient: ')
-        #获取卡对象
+        #Get Card Object
         cardobj=self.cardid_userobj_dict[cardid].card
-        #如果卡存在，则验证密码
+        #Verify password if card exists
         if self.__checkpwd(cardobj):
-            #验证是否锁卡
+            #Verify that the card is locked
             if cardobj.islock:
                 print('''
 **********************************************************
@@ -234,9 +234,9 @@ class Controller():
 ''')
                 return
             else:
-                #获取转账金额
+                #Get the transfer amount
                 transfer=int(input('Please enter your transfer amount: '))
-                #转出金额不能大于卡上现有余额
+                #The amount transferred cannot be greater than the existing balance on the card
                 if transfer>cardobj.balance:
                     print('''
 **************************
@@ -257,11 +257,11 @@ class Controller():
 *************************************************************
 ''')
 
-    #6.修改密码
+    #6.Change password
     def change_password(self):
-        #获取用户卡号
+        #Obtain user card number
         cardid=int(input('Please enter your card number: '))
-        #验证用户卡号是否存在
+        #Verify the existence of the user's card number
         if cardid not in self.cardid_userobj_dict:
             print('''
 ********************************************
@@ -272,7 +272,7 @@ class Controller():
 ''')
             return
         while True:
-            #让用户选择使用 身份证 或者 原密码 进行密码修改
+            #Let the user choose to use 【user ID】 or 【original password】 to change the password
             num_pwd=input('''
 ***************************************
 *                                     *
@@ -282,7 +282,7 @@ class Controller():
 
 Please select the mode to change your password:
 ''')
-            #验证用户输入的数字是否正确
+            #Verify that the numbers entered by the user are correct
             code_pwd=['1','2']
             if num_pwd not in code_pwd:
                 print('''
@@ -294,12 +294,12 @@ Please select the mode to change your password:
 ''')
                 #return
             if num_pwd=='1':
-                #选择使用【原密码】进行密码修改
-                #获取卡对象
+                #Select【Original Password】to change your password
+                #Get the Card Object
                 cardobj=self.cardid_userobj_dict[cardid].card
-                #先验证原密码
+                #Verify original password first
                 if self.__checkpwd(cardobj):
-                    #验证是否锁卡
+                    #Verify that the card is locked
                     if cardobj.islock:
                         print('''
 **********************************************************
@@ -310,7 +310,7 @@ Please select the mode to change your password:
 ''')
                         return
                     else:
-                        #设定新密码
+                        #Set a new password
                         newpwd=self.__getnewpwd(cardobj)
                         cardobj.pwd=newpwd
                         print(f'''
@@ -321,18 +321,17 @@ Please select the mode to change your password:
 *                                                  *
 ****************************************************
 ''')
-                        #跳出本次循环
+                        #Jump out of this loop
                         return
             if num_pwd=='2':
-                #选择使用【身份证】进行密码修改
-                #获取用户对象
+                #Select【User ID】to change your password
+                #Get the User Object
                 userobj=self.cardid_userobj_dict[cardid]
-                cardobj=userobj.card
-                #先验证身份证号码
+                #Verify user ID number first
                 if self.__checkuserid(userobj):
-                    #获取卡对象
+                    #Get the Card Object
                     cardobj=userobj.card
-                    #验证是否锁卡
+                    #Verify that the card is locked
                     if cardobj.islock:
                         print('''
 **********************************************************
@@ -343,7 +342,7 @@ Please select the mode to change your password:
 ''')
                         return
                     else:
-                        #设定新密码
+                        #Set a new password
                         newpwd=self.__getnewpwd(cardobj)
                         cardobj.pwd=newpwd
                         print(f'''
@@ -355,18 +354,18 @@ Please select the mode to change your password:
 ****************************************************
 ''')
                         return
-    #7.锁卡功能
+    #7.Card lock function
     def lockcard(self):
         print('Lock card ')
-    #8.解卡功能
+    #8.Card unlock function
     def unlockcard(self):
         print('Unlock card ')
-    #9.补卡功能
+    #9.Card replacement function
     def replacecard(self):
         print('Replace card ')
-    #0.退出
+    #0.Exit
     def exit(self):
-        #把当前数据，写入到文件中
+        #Write the current data, to a file
         with open(self.card_file_url,'wb+') as fp:
             pickle.dump(self.userid_cardobj_dict,fp)
         with open(self.user_file_url,'wb+') as fp:
@@ -379,7 +378,7 @@ Please select the mode to change your password:
 ****************
 ''')
 
-    #获取用户名的私有方法
+    #Private method for obtaining a username
     def __getusername(self):
         while True:
             name=input('Please enter your name: ')
@@ -388,7 +387,7 @@ Please select the mode to change your password:
                 continue
             else:
                 return name
-    #获取用户身份证号的私有方法
+    #Private method for obtaining a user ID
     def __getuserid(self):
         while True:
             userid=int(input('Please enter your ID: '))
@@ -397,7 +396,7 @@ Please select the mode to change your password:
                 continue
             else:
                 return userid
-    #获取用户电话号码的私有方法
+    #Private method for obtaining a user's phone number
     def __getphone_num(self):
         while True:
             phone_num=int(input('Please enter your phone number: '))
@@ -406,7 +405,7 @@ Please select the mode to change your password:
                 continue
             else:
                 return phone_num
-    #获取用户密码的私有方法
+    #Private method for obtaining the password
     def __getpwd(self):
         while True:
             pwd=int(input('Please enter your password: '))
@@ -414,7 +413,7 @@ Please select the mode to change your password:
                 print('The password you have entered is incorrect, please re-enter it: ')
                 continue
             else:
-                #密码输入正确后，再次输入确认密码
+                #Once the password has been entered correctly, confirm the password again
                 repwd=int(input('Please enter your password, again: '))
                 if repwd==pwd:
                     return pwd
@@ -428,7 +427,7 @@ Please select the mode to change your password:
 ''')
                     continue
 
-    #获取用户设置的新密码的方式
+    #Private method for obtaining a new password
     def __getnewpwd(self,cardobj):
         while True:
             newpwd=int(input('Please enter your new password: '))
@@ -442,7 +441,7 @@ Please select the mode to change your password:
 ''')
                 continue
             else:
-                #密码输入正确后，再次输入确认密码
+                #Once the password has been entered correctly, confirm the password again
                 renewpwd=int(input('Please enter your new password, again: '))
                 if renewpwd==newpwd:
                     return newpwd
@@ -456,19 +455,19 @@ Please select the mode to change your password:
 ''')
                     continue
 
-    #检测密码是否正确
+    #Check if the password is correct
     def __checkpwd(self,cardobj):
         num=3
         while True:
-            #获取密码
+            #Obtain the PIN
             pwd=int(input('Please enter your password: '))
-            #检测密码是否正确
+            #Check if the password is correct
             if pwd==cardobj.pwd:
                 return True
             else:
                 num=num-1
                 if num==0:
-                    #直接锁卡
+                    #card lock
                     cardobj.islock=True
                     print('''
 ************************
@@ -487,16 +486,16 @@ Please select the mode to change your password:
 **************************************************
 ''')
 
-    #检验身份证是否正确
+    #Check if the user ID card is correct
     def __checkuserid(self,userobj):
         while True:
-            #获取身份证号码
+            #Obtain user ID
             userid=int(input('Please enter your user ID number: '))
-            #检验身份证号码是否正确
+            #Check if the ID number is correct
             if userid==userobj.userid:
                 return True
             else:
-                #锁卡
+                #Card lock
                 self.userobj.card.islock=True
                 print('''
 ***************************************
